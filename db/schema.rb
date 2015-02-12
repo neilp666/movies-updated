e47b4375eaba86f03202a53e6d84cb7bbe8fcb91
neil patel <neil@myindianweddingapp.com>
2015-02-12 10:21:05 +0000

    Link reviews to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212100208) do
+ActiveRecord::Schema.define(version: 20150212101513) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150212100208) do
     t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
